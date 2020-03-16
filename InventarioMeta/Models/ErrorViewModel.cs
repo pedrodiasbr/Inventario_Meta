6f842360baa153112b48794f549f7b1efa969564
pedrodiasbr <pedro.dias@meta3.com.br>
2020-03-16 15:06:03 -0300

    first commit

---
+++ InventarioMeta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventarioMeta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
