3b4fcc1b8fedb7c86c6a8744cbc14565e128fe2c
pedrodiasbr <pedro.dias@meta3.com.br>
2020-03-17 09:25:18 -0300

    MCV login concluido, falta a parte do banco de dados

---
+++ InventarioMeta/Controllers/Autenticacao/AutenticacaoController.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+using System.Web;
+using InventarioMeta.Models;
+using InventarioMeta.Models.Autenticacao;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioMeta.Controllers
+{
+    [AllowAnonymous]
+    public class AutenticacaoController : Controller
+    {
+        public ActionResult Login(string urlRetorno)
+        {
+            var model = new LoginModel
+            {
+                UrlRetorno = urlRetorno
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+       
+    }
+       
+}
---
+++ InventarioMeta/Models/Autenticacao/LoginModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventarioMeta.Models.Autenticacao
+{
+    public class LoginModel
+    {
+        
+        public string UrlRetorno { get; set; }
+
+        
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Confirme a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar senha")]
+        [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres")]
+        [Compare(nameof(Senha), ErrorMessage = "A senha e a confirmação não estão iguais")]
+        [MaxLength(20)]
+        public string ConfirmacaoSenha { get; set; }
+
+        [Required(ErrorMessage = "Informe o login")]
+        [Display(Name = "Login")]
+        [MaxLength(100)]
+        [DataType(DataType.Text)]
+        public string Login { get; set; }
+
+       
+    }
+}
